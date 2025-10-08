287c99b91c6a032f5eddb5a61edcd976306c6c97
Rullzsy HUB <rullzsyhub@gmail.com>
2025-10-08 23:46:23 +0700

    Update main.lua

--- RullzsyHUB - MOUNT YNTKTS/main.lua
+++ RullzsyHUB - MOUNT YNTKTS/main.lua
@@ -1276,7 +1276,7 @@ local Toggle = AutoWalkTab:CreateToggle({
 -- Slider Speed Auto
 local SpeedSlider = AutoWalkTab:CreateSlider({
     Name = "⚡ Set Speed",
-    Range = {0.5, 4},
+    Range = {0.5, 1.5},
     Increment = 0.10,
     Suffix = "x Speed",
     CurrentValue = 1,
