c1bdc85e0604acf08533e8ed080758a290136336
Rullzsy HUB <rullzsyhub@gmail.com>
2025-10-11 14:18:19 +0700

    Update main.lua

--- RullzsyHUB - MOUNT YACAPE/main.lua
+++ RullzsyHUB - MOUNT YACAPE/main.lua
@@ -1268,7 +1268,7 @@ local Toggle = AutoWalkTab:CreateToggle({
 -- Slider Speed Auto
 local SpeedSlider = AutoWalkTab:CreateSlider({
     Name = "⚡ Set Speed",
-    Range = {0.5, 4},
+    Range = {0.5, 1.5},
     Increment = 0.10,
     Suffix = "x Speed",
     CurrentValue = 1.0,
@@ -1846,5 +1846,6 @@ CreditsTab:CreateLabel("UI: Rayfield Interface")
 CreditsTab:CreateLabel("Dev: RullzsyHUB")
 -------------------------------------------------------------
 -- CREDITS - END
+
 
 -------------------------------------------------------------
