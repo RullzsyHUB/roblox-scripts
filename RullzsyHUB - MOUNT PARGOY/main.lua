8804ee9a45a4828097bdf6f780a0b1555fdb79d3
Rullzsy HUB <rullzsyhub@gmail.com>
2025-10-14 06:21:24 +0700

    Update main.lua

--- RullzsyHUB - MOUNT PARGOY/main.lua
+++ RullzsyHUB - MOUNT PARGOY/main.lua
@@ -1366,7 +1366,7 @@ local Toggle = AutoWalkTab:CreateToggle({
 -- Slider Speed Auto
 local SpeedSlider = AutoWalkTab:CreateSlider({
     Name = "⚡ Set Speed",
-    Range = {0.5, 2},
+    Range = {0.5, 1.2},
     Increment = 0.10,
     Suffix = "x Speed",
     CurrentValue = 1.0,
@@ -2092,4 +2092,5 @@ CreditsTab:CreateLabel("Dev: RullzsyHUB")
 
 
 -------------------------------------------------------------
+
 
